2c585cb5567d47f416562011964c47037133908f
Yunyu Lin <mail@yunyul.in>
2020-08-06 09:43:45 -0700

    Move setTimeout to bundle

--- src/hookInputs.ts
+++ src/hookInputs.ts
@@ -2,9 +2,7 @@ const hookInputSetter = <T extends HTMLElement>(target: T, key: string) => {
   const orig = Object.getOwnPropertyDescriptor(target, key);
   Object.defineProperty(target, key, {
     set(value) {
-      setTimeout(() => {
-          (this as T).dispatchEvent(new Event("coherevalueupdate"));
-      });
+      (this as T).dispatchEvent(new Event("coherevalueupdate"));
       if (orig && orig.set) {
         orig.set.call(this, value);
       }
