8a1bdc765bfe0ff65af4908ada2fa77b26a09fb4
Yunyu Lin <mail@yunyul.in>
2021-06-24 09:55:52 -0400

    Safer hookSegment

--- src/hookSegment.ts
+++ src/hookSegment.ts
@@ -4,11 +4,14 @@ const hookSegment = () => {
   if (window.analytics && !hooked) {
     hooked = true;
     const origIdentify = window.analytics.identify.bind(window.analytics);
-    window.analytics.identify = (...args: any[]) => {
-      const userId = args[0];
+    window.analytics.identify = (...args: unknown[]) => {
+      const userId = args[0] as string;
       // Ignore anonymous users
       if (typeof userId === "string") {
-        const traits = typeof args[1] === "object" ? args[1] : {};
+        const traits =
+          args[1] && typeof args[1] === "object"
+            ? (args[1] as Record<string, unknown>)
+            : {};
         // Rename name to displayName
         const { name: displayName, ...rest } = traits;
         window.Cohere.identify(userId, { displayName, ...rest });
